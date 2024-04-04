8a6f8be2235705ba9ea895f85fa593f719ea290f
mingzhi-liu1258 <mingzhi.liu@salesforce.com>
2024-04-04 16:57:37 +0900

    add heroku postgres

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
+
 const express = require('express')
 const path = require('path')
 
@@ -8,4 +17,16 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
